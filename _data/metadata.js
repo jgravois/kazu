f0cc4ecd8dbb6dd10aac8059dc33dbc0d643b033
John Gravois <jgravois@compiler.la>
2026-06-12 07:47:26 -0700

    chore: cleanup/setup

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Kazu Podcast",
+	url: "https://kazu-podcast.netlify.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Theo and Matt homage.",
 	author: {
-		name: "Your Name Here",
+		name: "Mr. Smithers",
 		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
--- eleventy.config.js
+++ eleventy.config.js
@@ -152,17 +152,4 @@ export const config = {
 		data: "../_data",          // default: "_data" (`input` relative)
 		output: "_site"
 	},
-
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
-
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
-
-	// pathPrefix: "/",
 };
