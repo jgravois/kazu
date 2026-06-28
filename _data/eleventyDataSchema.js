9d10f01686fe73c5bf9fe12b42695782644969a7
John Gravois <jgravois@compiler.la>
2026-06-28 10:36:52 -0700

    chore: one less dep

--- _data/eleventyDataSchema.js
+++ _data/eleventyDataSchema.js
@@ -1,17 +1,11 @@
 import { z } from "zod";
-import { fromZodError } from 'zod-validation-error';
 
 // Draft content, validate `draft` front matter
 export default function() {
 	return function(data) {
 		// Note that drafts may be skipped in a preprocessor (see eleventy.config.js)
 		// when doing a standard build (not --serve or --watch)
-		const result = z.object({
-			draft: z.optional(z.boolean())
-		}).safeParse(data);
-
-		if(result.error) {
-			throw fromZodError(result.error);
-		}
+		const result = z.object({ draft: z.optional(z.boolean()) }).safeParse(data);
+		if(result.error) throw result.error;
 	}
 }
